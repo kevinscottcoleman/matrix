e9e1efde2c9481deae290061d5f0f70ca8293d82
Jan Jaap <jan.jaap@digimondo.de>
2024-10-27 20:30:02 +0100

    feat(coolify): fix env var names in population script

--- scripts/populate_from_env.sh
+++ scripts/populate_from_env.sh
@@ -15,7 +15,7 @@ SMTP_PORT="${SMTP_PORT:-587}"
 SMTP_USER="${SMTP_USER:-your-email@address.com}"
 SMTP_PASS="${SMTP_PASS:-your-email-password}"
 SMTP_REQUIRE_TRANSPORT_SECURITY="${SMTP_REQUIRE_TRANSPORT_SECURITY:-true}"
-SMTP_NOTIF_FROM="${SMTP_NOTIF_FROM:-your-from-address@address.com}"
+SMTP_NOTIFY_FROM="${SMTP_NOTIFY_FROM:-your-from-address@address.com}"
 SMTP_APP_NAME="${SMTP_APP_NAME:-Attraktor Matrix}"
 
 # Extract environment variables for OIDC configuration
@@ -47,7 +47,7 @@ sed -e "s|your.email.server|$SMTP_HOST|g" \
     -e "s|your-email@address.com|$SMTP_USER|g" \
     -e "s|your-email-password|$SMTP_PASS|g" \
     -e "s|true|$SMTP_REQUIRE_TRANSPORT_SECURITY|g" \
-    -e "s|your-from-address@address.com|$SMTP_NOTIF_FROM|g" \
+    -e "s|your-from-address@address.com|$SMTP_NOTIFY_FROM|g" \
     -e "s|Attraktor Matrix|$SMTP_APP_NAME|g" \
     ./synapse_data/config/email.yaml.example > ./synapse_data/config/email.yaml
 
