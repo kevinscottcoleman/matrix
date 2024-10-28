e0fae18579fa171a15b2308922af9cbf6ecb6f58
Jan Jaap <jan.jaap@digimondo.de>
2024-10-28 20:22:24 +0100

    feat(synapse-mas): make everything configurable through envs

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,18 +1,7 @@
-# Install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
-# Load nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Install node 20
-nvm install v20.15.1
-nvm use v20.15.1
-
-# Install npx
-npm i -g npx
+# Install deno
+curl -fsSL https://deno.land/install.sh | sh
 
 ls -la ./scripts
 
-# run script using ts-node
-npx ts-node ./scripts/index.ts
+# run script using deno
+deno -R -W ./scripts/index.ts
