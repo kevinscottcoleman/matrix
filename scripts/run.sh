8c74b01228e1a7df9bae5ebe5c74f15580d78273
Jan Jaap <jan.jaap@digimondo.de>
2024-10-28 20:18:13 +0100

    feat(synapse-mas): make everything configurable through envs

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 nvm install v20.15.1
 nvm use v20.15.1
+npm i -g npx
 ls -la ./scripts
 npx ts-node ./scripts/index.ts
