709e929076110e3b3274f4a2d987d7f88a90f00b
Jan Jaap <jan.jaap@digimondo.de>
2024-10-28 21:48:17 +0100

    feat(synapse-mas): make everything configurable through envs

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+function loadEnvFileIntoEnvVars() {
+    const envFilePath = path.join(__dirname, ".env");
+    if (!fs.existsSync(envFilePath)) {
+        throw new Error(`.env file not found: ${envFilePath}`);
+    }
+
+    const envFile = fs.readFileSync(envFilePath, "utf8");
+    envFile.split("\n").forEach((line) => {
+        const [key, value] = line.split("=");
+        process.env[key] = value;
+    });
+}
+
+loadEnvFileIntoEnvVars();
+
 const {populateSynapseMasConfig } = require("./populate_synapse_mas_config");
 const { populateConfigsFromEnv } = require("./populate_configs_from_env");
 
