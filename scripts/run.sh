e532117d2734d12f473a440151e4fc87a05ed494
Jan Jaap <jan.jaap@digimondo.de>
2024-10-28 21:28:11 +0100

    feat(synapse-mas): make everything configurable through envs

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Echo the current working directory
-echo "Current directory: $(pwd)"
-ls -la .
+apk add --no-cache node
 
-# Define the Node.js version you want to use
-NODE_VERSION=20
-
-# Run the Docker container using the official Node.js image
-#docker run --rm -v "$(pwd)":/usr/src/app -w /usr/src/app node:$NODE_VERSION node ./scripts/index.js
-docker run --rm -v $(pwd)/scripts:/home/node/jappy node:$NODE_VERSION ls -la /home/node/jappy
+node ./scripts/index.js
