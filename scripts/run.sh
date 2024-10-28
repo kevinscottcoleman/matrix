c2c981aeec2585cc16e535e0ef1a9a391fc3451a
Jan Jaap <jan.jaap@digimondo.de>
2024-10-28 20:12:12 +0100

    feat(synapse-mas): make everything configurable through envs

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 curl -fsSL https://bun.sh/install | bash
 export BUN_INSTALL="$HOME/.bun" 
 export PATH="$BUN_INSTALL/bin:$PATH"
-~/.bun/bin/bun run ./scripts/index.ts
+~/.bun/bin/bun ./scripts/index.ts
