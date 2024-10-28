58a763715a1f543371b548ef9199ad02aa777710
Jan Jaap <jan.jaap@digimondo.de>
2024-10-28 21:08:37 +0100

    feat(synapse-mas): make everything configurable through envs

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,4 +8,4 @@ NODE_VERSION=20
 
 # Run the Docker container using the official Node.js image
 #docker run --rm -v "$(pwd)":/usr/src/app -w /usr/src/app node:$NODE_VERSION node ./scripts/index.js
-docker run --rm -v "$(pwd)":/usr/src/app -w /usr/src/app node:$NODE_VERSION ls -la /app
+docker run --rm -v "$(pwd)":/usr/src/app -w /usr/src/app node:$NODE_VERSION ls -la /usr/src/app
