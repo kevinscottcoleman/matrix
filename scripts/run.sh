1969e2b699a682a8072a0fb32b76bf92a095cb98
Jan Jaap <jan.jaap@digimondo.de>
2024-10-28 20:28:28 +0100

    feat(synapse-mas): make everything configurable through envs

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,5 +9,8 @@ pwd
 # ls -la on output of pwd
 ls -la
 
-# run script using deno
-/root/.deno/bin/deno -R -W ./scripts/index.ts
+# run script (pwd + scripts/index.ts) using deno
+export SCRIPT_PATH=$(pwd)/scripts/index.ts
+echo "Running script: $SCRIPT_PATH"
+echo $SCRIPT_PATH
+/root/.deno/bin/deno --allow-read --allow-write --allow-env --allow-run --allow-hrtime --no-check $SCRIPT_PATH
