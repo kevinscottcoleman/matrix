73bae125eb22983c7777cc07762b4100d7957c10
Jan Jaap <jan.jaap@digimondo.de>
2024-10-28 20:56:48 +0100

    feat(synapse-mas): make everything configurable through envs

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Change directory to where the Dockerfile is located
-# its the current working dir plus /scripts
-SCRIPT_DIR="$(pwd)/scripts"
-
 echo "Current directory: $SCRIPT_DIR"
 
 # Build the Docker image using the Dockerfile in the scripts directory
-docker build -t matrix-env-populater -f "$SCRIPT_DIR/Dockerfile" .
+docker build -t matrix-env-populater -f "./scripts/Dockerfile" .
 
 # Run the Docker container
 docker run --rm -v "$(pwd)":/usr/src/app matrix-env-populater
