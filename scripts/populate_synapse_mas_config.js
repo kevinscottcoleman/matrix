26763a0cd66a2688facc86fae1f0c857eb04afe7
Jan Jaap <jan.jaap@digimondo.de>
2024-10-29 09:59:27 +0100

    feat(synapse-mas): make everything configurable through envs

--- scripts/populate_synapse_mas_config.js
+++ scripts/populate_synapse_mas_config.js
@@ -12,14 +12,20 @@ let configContents = readFileSync(
   "utf8"
 );
 
-const SYNAPSE_HOST = getEnv("SYNAPSE_FQDN").replace('http://', 'https://').replace('https://', '');
+const SYNAPSE_HOST = getEnv("SYNAPSE_FQDN")
+  .replace("http://", "https://")
+  .replace("https://", "");
 
 const replacements = [
   {
     search: "uri: postgresql://",
-    replace: `uri: postgresql://${encodeURIComponent(getEnv("POSTGRES_SYNAPSE_MAS_USER"))}:${encodeURIComponent(getEnv(
-      "POSTGRES_SYNAPSE_MAS_PASSWORD"
-    ))}@postgres-synapse-mas:5432/${encodeURIComponent(getEnv("POSTGRES_SYNAPSE_MAS_DB"))}`,
+    replace: `uri: postgresql://${encodeURIComponent(
+      getEnv("POSTGRES_SYNAPSE_MAS_USER")
+    )}:${encodeURIComponent(
+      getEnv("POSTGRES_SYNAPSE_MAS_PASSWORD")
+    )}@postgres-synapse-mas:5432/${encodeURIComponent(
+      getEnv("POSTGRES_SYNAPSE_MAS_DB")
+    )}`,
   },
   {
     search: `from: '"Authentication Service" <root@localhost>'`,
@@ -41,14 +47,40 @@ const replacements = [
     search: "endpoint: http://localhost:8008/",
     replace: `endpoint: http://${getEnv("SYNAPSE_FQDN")}/`,
   },
+  {
+    search: /matrix:\n  homeserver: (.*)\n.*secret: (.*)/,
+    replace: (match, homeserver, secret) => {
+      const newSecret = getEnv("SYNAPSE_API_ADMIN_TOKEN");
+      return `matrix:\n  homeserver: ${homeserver}\n  secret: ${newSecret}`;
+    },
+    useRegex: true,
+  },
 ];
+
+function replaceByRegex(text, searchRegex, replaceFunction) {
+  return text.replace(searchRegex, replaceFunction);
+}
 
 function populateSynapseMasConfig() {
   replacements.forEach((replacement) => {
-    configContents = configContents
-      .split(replacement.search)
-      .join(replacement.replace);
+    if (replacement.useRegex) {
+      configContents = replaceByRegex(
+        configContents,
+        replacement.search,
+        replacement.replace
+      );
+    } else {
+      configContents = configContents
+        .split(replacement.search)
+        .join(replacement.replace);
+    }
   });
+
+  configContents += `
+clients:
+  - client_id: 0000000000000000000SYNAPSE
+    client_auth_method: client_secret_basic
+    client_secret: "${getEnv("SYNAPSE_MAS_SECRET")}"`;
 
   writeFileSync("./configurations/synapse-mas/config.yaml", configContents);
 }
