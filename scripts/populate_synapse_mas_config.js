ee7f1551131e7d2550cfa3066c6162461c382b18
Jan Jaap <jan.jaap@digimondo.de>
2024-10-29 10:49:15 +0100

    feat(synapse-mas): sso config fixup

--- scripts/populate_synapse_mas_config.js
+++ scripts/populate_synapse_mas_config.js
@@ -81,11 +81,11 @@ const replacements = [
   },
   {
     search: "public_base: http://[::]:8080/",
-    replace: `public_base: ${getEnv(SYNAPSE_MAS_FQDN)}`,
+    replace: `public_base: ${getEnv("SYNAPSE_MAS_FQDN")}`,
   },
   {
     search: "issuer: http://[::]:8080/",
-    replace: `issuer: ${getEnv(SYNAPSE_MAS_FQDN)}`,
+    replace: `issuer: ${getEnv("SYNAPSE_MAS_FQDN")}`,
   },
 ];
 
