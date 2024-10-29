149489a8d555d84d5f244106727bd0546fee5277
Jan Jaap <jan.jaap@digimondo.de>
2024-10-29 09:23:22 +0100

    feat(synapse-mas): make everything configurable through envs

--- scripts/populate_synapse_mas_config.js
+++ scripts/populate_synapse_mas_config.js
@@ -17,9 +17,9 @@ const SYNAPSE_HOST = getEnv("SYNAPSE_FQDN").replace('http://', 'https://').repla
 const replacements = [
   {
     search: "uri: postgresql://",
-    replace: `uri: postgresql://${getEnv("POSTGRES_SYNAPSE_MAS_USER")}:${getEnv(
+    replace: `uri: postgresql://${encodeURIComponent(getEnv("POSTGRES_SYNAPSE_MAS_USER"))}:${encodeURIComponent(getEnv(
       "POSTGRES_SYNAPSE_MAS_PASSWORD"
-    )}@postgres-synapse-mas:5432/${getEnv("POSTGRES_SYNAPSE_MAS_DB")}`,
+    ))}@postgres-synapse-mas:5432/${encodeURIComponent(getEnv("POSTGRES_SYNAPSE_MAS_DB"))}`,
   },
   {
     search: `from: '"Authentication Service" <root@localhost>'`,
